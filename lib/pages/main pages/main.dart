24fd46a33b6826b20d7ce92f3c2c2b0eb2574cea
Abhiram ms <abhiramsailu30@gmail.com>
2022-08-30 21:29:02 +0530

    to do list app using flutter and firebase this project is  made for studying purpose still in developement

--- lib/pages/main pages/main.dart
+++ lib/pages/main pages/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:todo/pages/ui%20pages/Home.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:todo/pages/ui%20pages/Login.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -16,7 +19,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: const Home(),
+      home: const Login(),
     );
   }
 }
